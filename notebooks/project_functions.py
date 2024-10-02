e211ea6594341d09c3f32949c138767c32fc69ff
David Moreno <124578675+davidmoreno-gh@users.noreply.github.com>
2024-10-02 10:11:31 +0200

    Revisión y limpieza Puntos 1 al 4

--- notebooks/project_functions.py
+++ notebooks/project_functions.py
@@ -3,9 +3,25 @@
 def clean_my_columns(df):
     df.columns = df.columns.str.replace(" ", "_").str.lower().str.strip()
     return df
+'''
+def find_duplicates(df):
+    duplicates = df.duplicated(keep=False)
+    df_duplicates = df[duplicates]
+    return df_duplicates'''
 
-
+def find_duplicates(df):
+    duplicates = df.duplicated(keep=False)
+    df_duplicates = df[duplicates]
+    df_no_duplicates = df.drop_duplicates(keep=False)
+    return df_duplicates, df_no_duplicates
     
+def find_and_drop_null_rows(df):
+    null_rows = df.isnull().all(axis=1)
+    df_null_rows = df[null_rows]
+    df_no_null_rows = df.dropna(how='all')
+    return df_null_rows, df_no_null_rows
+
+
 # function to calculate a cross table with rate of a given column with binary data (0-1). 
 
 def create_cross_tab(df, stat):
